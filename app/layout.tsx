8a7e592e870cc24b94df71b79c687356f293be4a
Anagho <anaghosamroyce@gmail.com>
2025-12-07 02:37:32 +0100

    changed icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "conferex",
   description: "conferex online meeting platform",
   icons: {
-    icon: '/icons/icon.png'
+    icon: '/icons/icon.svg'
   }
 };
 
